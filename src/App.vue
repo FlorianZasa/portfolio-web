940e7588544fff1b156ab1a41ea1323f42f170d0
FlorianZasada <florian.zasada@gmail.com>
2022-10-31 19:22:55 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="window">
+    <div id="nav">
+      <router-link class="router-link" to="/">Home</router-link>
+      <router-link class="router-link" to="/about">Über mich</router-link>
+      <router-link class="router-link" to="/contact">Kontakt</router-link>
+    </div>
+    <router-view class="content"/>
   </div>
-  <router-view/>
+  <footer>
+    <p>© 2022 Florian Zasada</p>
+  </footer>
 </template>
 
 <style>
@@ -11,20 +17,53 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: #2c3e50;
+}
+
+.content {
+  padding: 3vw;
+  width: 78vw
+}
+
+.window {
+  display: inline-flex;
+  height: 100%
 }
 
 #nav {
-  padding: 30px;
+  padding-top: 5vw;
+  width: 22vw;
+  background-color: #25313e;
+  border-radius: 0 0 20px 0;
+  display:flex;
+  flex-direction: column;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+footer {
+  padding: 3vw;
+  display: flex;
+  justify-content: center;
+  background: #2c3948;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#nav .router-link {
+  color: var(--secondary-fg);
+  font-size: 2vw;
+  width: 100%;
+  border-bottom: 2px solid #4c5053;
+  text-align: center;
+  margin: 10px;
+  
+  background: linear-gradient(to left, #25313e, rgb(107, 119, 59) 99%) right;
+  background-size: 200% 100%;
+  position: relative;
 }
+
+#nav .router-link:hover {
+  transition: all 1s ease-in-out;
+  background-position: left;
+  color: var(--fg-text);
+  width: 120%;
+}
+
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+    <div class="home">
+      <div class="header-frame">
+          <h1>Erfahre mehr</h1>
+          <h2>über mich</h2>
+      </div>
+      <div class="spacer"></div>
+      
+      <div class="subtitle-frame">
+        <h4>Hier steht ein langer Text drin.</h4>
+      </div>
+      <div class="spacer"></div>
+  
+      
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  
+  export default {
+    name: 'Home',
+  }
+  </script>
+  
+  <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap');
+  
+  .home {
+    align-items: center;
+    text-align: center;
+  }
+  
+  .header-frame {
+    align-items: center;
+    width: 100%; 
+  }
+  
+  .spacer{
+    padding-bottom: 4vw;
+  }
+  
+  .title-frame {
+    align-items: center;
+    width: 60%; 
+  }
+  
+  .title-frame img {
+    width: 100%;
+  }
+  
+  .fancy {
+    font-weight: bold;
+    font-family: 'Roboto Mono', monospace;
+    color: var(--secondary-fg);
+    font-weight: bold;
+  }
+  
+  .image-frame {
+    width: 40%;
+    padding: 0px;
+    margin: 0px;
+  }
+  .image-frame img {
+    width: 50%;
+    height: auto;
+    border:4px solid var(--secondary-fg);
+    border-radius: 20px;
+  }
+  </style>
+  
